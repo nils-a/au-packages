ea03c5dbfdbe8ebbac3d2886bfe3656cfc836de0
Nils Andresen <nils@nils-andresen.de>
2020-07-24 10:51:23 +0200

    initial idea (#6)

---
+++ cleanmgrplus/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'cleanmgrplus'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/builtbybel/CleanmgrPlus/releases/download/1.34.1074/cleanmgr+.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  softwareName  = 'cleanmgrplus*'
+  url           = $url
+
+  checksum      = '77595781DBE064404E0D1725F98FF894ECD990B6BDA85F3CA4A571231ED64952'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+Write-Verbose "Adding Desktop-Link"
+$target = Join-Path -Path $toolsDir -ChildPath 'Cleanmgr+.exe'
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path -Path $desktop -ChildPath 'Cleanmgr+.lnk'
+Install-ChocolateyShortcut `
+   -ShortcutFilePath $shortcut `
+   -TargetPath $target
---
+++ cleanmgrplus/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Write-Verbose "Removing Desktop-Link"
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path -Path $desktop -ChildPath 'Cleanmgr+.lnk'
+if(Test-Path $shortcut) {
+  Remove-Item $shortcut
+}
