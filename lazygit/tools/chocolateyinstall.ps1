5f94d8446ee4b49b43d577691262d64de5ca738b
Chocolatey <chocolatey@realdimensions.net>
2021-04-10 07:43:30 +0000

    AU: 1 updated - lazygit [skip ci] https://gist.github.com/be944da6cbb50300f2e3114bf35724a0/0ae063b5045f34e60d51ec35d404719bb546ae12

--- lazygit/tools/chocolateyinstall.ps1
+++ lazygit/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url           = 'https://github.com/jesseduffield/lazygit/releases/download/v0.27.1/lazygit_0.27.1_Windows_32-bit.zip'
-    url64bit      = 'https://github.com/jesseduffield/lazygit/releases/download/v0.27.1/lazygit_0.27.1_Windows_x86_64.zip'
-    checksum      = 'D8870233401712DDB21B4391D9CC380CEB4D7B170F736CFA6A9EFA7B1BAB11A0'
-    checksum64    = 'C11D8FB3649E8C4BA5A8E84EECED0A841F11AB1B3600E4E1BC2B9AF4C5BE6B6A'
+    url           = 'https://github.com/jesseduffield/lazygit/releases/download/v0.27.3/lazygit_0.27.3_Windows_32-bit.zip'
+    url64bit      = 'https://github.com/jesseduffield/lazygit/releases/download/v0.27.3/lazygit_0.27.3_Windows_x86_64.zip'
+    checksum      = '00EE4D3A3885FEEBCD4B6967366EE0E0220F885E7011986BF8876FE653AB6D2C'
+    checksum64    = '20CF183AF53112C6F515553355D8CB54399555666D27CE6D8EC3E2AA8CD5DA7D'
     checksumType  = 'sha256'
 }
 
