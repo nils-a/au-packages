bb8ae6546aea45a0d126b61a628125d6e8a24981
Nils Andresen <nils@nils-andresen.de>
2021-08-31 11:36:50 +0200

    New package: JetBrainsMonoNF

---
+++ JetBrainsMonoNF/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installLog = Join-Path -Path $toolsDir -ChildPath "installed.txt"
+
+# if the package was force-installed multiple times
+# the list of installed fonts will be empty and uninstall will 
+# not remove all that was installed.
+# Hence, I hard-coded the current font-names here for backup.
+$staticNames = @(
+ "JetBrains Mono Bold Italic Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Bold Italic Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Bold Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Bold Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono ExtBd Ita Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono ExtBd Ita Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Extra Bold Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Extra Bold Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono ExtraBold ExBd I Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono ExtraBold ExBd I Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono ExtraBold ExtBd Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono ExtraBold ExtBd Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Italic Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Italic Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Medium Italic Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Medium Italic Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Medium Med Ita Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Medium Med Ita Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Medium Medium Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Medium Medium Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Medium Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Medium Nerd Font Complete Windows Compatible.ttf",
+ "JetBrains Mono Regular Nerd Font Complete Mono Windows Compatible.ttf",
+ "JetBrains Mono Regular Nerd Font Complete Windows Compatible.ttf"
+)
+
+$toUninstall = @()
+if(Test-Path $installLog) {
+  $toUninstall = Get-Content $installLog -Encoding unicode
+}
+
+
+if($toUninstall.length -lt 1) {
+  $toUninstall = $staticNames
+}
+
+$removed = Uninstall-ChocolateyFont $toUninstall -Multiple
+Write-Debug "Removed $removed previously installed fonts."
---
+++ JetBrainsMonoNF/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installLog = Join-Path -Path $toolsDir -ChildPath "installed.txt"
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    url           = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip'
+    checksum      = '842013FA44B6896D4EB91635A81EF75244D78D7F61FF866C9DFD3315A67788CD'
+    checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$fontsFolder = (New-Object -ComObject Shell.Application).namespace(0x14).self.path
+$nerdFontsBeforeInstall = Get-ChildItem -Path $fontsFolder -Filter "JetBrains Mono*Nerd Font*.ttf" | select -ExpandProperty Name
+
+$fonts = Get-ChildItem $toolsDir -Filter "*Windows Compatible.ttf"
+$paths = $fonts | select -ExpandProperty FullName
+$names = $fonts | select -ExpandProperty Name
+Write-Debug "Installing the following fonts`r`n - $($names -join "`r`n - ")"
+
+$added = Install-ChocolateyFont $paths -Multiple
+Write-Debug "Installed $added fonts."
+
+$nerdFontsAfterInstall = Get-ChildItem -Path $fontsFolder -Filter "JetBrains Mono*Nerd Font*.ttf" | select -ExpandProperty Name
+$newlyInstalledFonts = $nerdFontsAfterInstall | Where-Object { $nerdFontsBeforeInstall -notcontains $_ }
+$newlyInstalledFonts | Out-File $installLog -Encoding unicode -Append
