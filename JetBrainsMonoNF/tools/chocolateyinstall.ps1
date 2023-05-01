92f424ea45d5e99631a263553a04da74d3b7347a
Chocolatey <chocolatey@realdimensions.net>
2023-05-01 07:40:33 +0000

    AU: 1 updated - JetBrainsMonoNF [skip ci] https://gist.github.com/nils-a/be944da6cbb50300f2e3114bf35724a0/ec6a343c0aa51f7dcc4418d3ee948f0a6a8b15d1

--- JetBrainsMonoNF/tools/chocolateyinstall.ps1
+++ JetBrainsMonoNF/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $installLog = Join-Path -Path $toolsDir -ChildPath "installed.txt"
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url           = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip'
-    checksum      = '187C316BB77ED1C824099FE8621A17D14F7DE8A6CE5B14A1A04DE0AE674B4AA6'
+    url           = 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip'
+    checksum      = 'CCAA9C2625A20B47BC253CCD1D59A6F726CEE8922D5B9B52C197364D36E9EEEB'
     checksumType  = 'sha256'
 }
 
