6dd05f0142db4c3e07639b2ab2aa2d3c7eec7192
Nils Andresen <nils@nils-andresen.de>
2023-06-13 08:46:52 +0200

    [printrun] initial commit

---
+++ printrun/readme.md
@@ -0,0 +1,5 @@
+# printrun-chocolatey
+This is a simple package to install [printrun](https://www.pronterface.com/) for [chcoclatey](https://chocolatey.org/)
+
+## Description
+Printrun for windows ships only pronterface.exe and not the other tools that are part of the printrun suite.
---
+++ printrun/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'printrun'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/kliment/Printrun/releases/download/printrun-2.0.1/printrun-2.0.1_windows_x64_py3.10.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  softwareName  = 'printrun*'
+  url           = $url
+
+  checksum      = '3A35834A2F84A985CB36164BA18CA5E886E971C127A961D56721AE7D44582D92'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Verbose "Adding Desktop-Link"
+$target = Join-Path -Path $toolsDir -ChildPath 'pronterface.exe'
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path -Path $desktop -ChildPath 'pronterface.exe'
+Install-ChocolateyShortcut `
+   -ShortcutFilePath $shortcut `
+   -TargetPath $target
