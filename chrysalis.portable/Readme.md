fe7b8fe79cf40b97a8f69c38fb0b69e6a38651d4
Nils Andresen <nils@nils-andresen.de>
2023-07-19 13:50:59 +0200

    add portable chrysalis

---
+++ chrysalis.portable/Readme.md
@@ -0,0 +1,7 @@
+# Cleanmgr+ -chocolatey
+This is a simple package to install the portable version of [Chrysalis](https://github.com/keyboardio/Chrysalis/releases/) for [chcoclatey](https://chocolatey.org/)
+
+## Description
+Chrysalis is a graphical configurator for Kaleidoscope-powered keyboards. While still under development, it is already usable for a number of tasks. To try it, you don't even need to flash Kaleidoscope beforehand, Chrysalis can do that for you, as it ships with reasonable default firmware for each supported board.
+
+The primary purpose of the application is to allow one to configure their keyboard without having to compile or flash firmware, by storing the configuration on the keyboard itself, in EEPROM. There are no external tools required, just Chrysalis itself.
---
+++ chrysalis.portable/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+. $PSScriptRoot\..\chrysalis\update.ps1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^\s*\`$url\s*=\s*)('.*')"= "`$1'$($Latest.URL32)'"
+            "(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+        #".\legal\VERIFICATION.txt" = @{
+        #  "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+        #  "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        #  "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
+        #}
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+
+    $releases = Get-LatestGithubReleases -repoUser "keyboardio" -repoName "Chrysalis"
+    $stable = $releases.latestStable | select -First 1
+
+    $url = $stable.Assets | Where { $_ -like "*Chrysalis-win32-x64-*.zip" } | select -First 1
+
+    return @{
+        URL32        = $url
+        Version      = $stable.Version
+        ReleaseNotes = $stable.ReleaseUrl
+    }
+}
+
+update-package -ChecksumFor none -NoReadme
