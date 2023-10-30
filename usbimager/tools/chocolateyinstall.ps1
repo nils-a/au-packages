aef52e137066eb3dcbb6f154c37c416634358d67
Nils Andresen <nils@nils-andresen.de>
2023-10-30 09:14:20 +0100

    [skip-ci] usbimager: upstream changed the binary without a release.

--- usbimager/tools/chocolateyinstall.ps1
+++ usbimager/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
 
   softwareName  = 'usbimager*'
-  checksum      = '9200FBAAA617856692A0A59ED80492E3BFC5B4D32F0A3FB199B80C6C5CA2BC47'
+  checksum      = 'ad83b75c186994f732d913b27394e86214296f708a26021b70e561004e4499f7'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
