0281e2f9f0288336eec95d42084d05fc0bd71ef5
Chocolatey <chocolatey@realdimensions.net>
2023-12-13 07:40:18 +0000

    AU: 1 updated - colour-contrast-analyser [skip ci] https://gist.github.com/nils-a/be944da6cbb50300f2e3114bf35724a0/b026666dd8b1848d1c49f29b24628aa1de07d800

--- colour-contrast-analyser/tools/chocolateyinstall.ps1
+++ colour-contrast-analyser/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ThePacielloGroup/CCAe/releases/download/v3.4.2/CCA-Portable-x64-3.4.2.exe' 
+$url        = 'https://github.com/ThePacielloGroup/CCAe/releases/download/v3.5.1/CCA-Portable-x64-3.5.1.exe' 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
 
   softwareName  = 'colour-contrast-analyser*'
 
-  checksum      = 'B33EEDDD5CA5E46853935A03002AFD437C12F8B8A0CE1E2D2CDAE9C83CF3F982'
+  checksum      = 'E7767A611B28BE00C52CEA73E7FF35BE4A64B9A2E6B4756D11C15D25C93FC578'
   checksumType  = 'sha256' 
 }
 
