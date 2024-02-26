323f1b20e2b7d8abf27b67a13259f7cc8d511c2c
Chocolatey <chocolatey@realdimensions.net>
2024-02-26 07:40:30 +0000

    AU: 1 updated - smtp4dev [skip ci] https://gist.github.com/nils-a/be944da6cbb50300f2e3114bf35724a0/a24fd1ce2d7324458ffb9a0d7e2e45ec182ba6f2

--- smtp4dev/tools/chocolateyinstall.ps1
+++ smtp4dev/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'smtp4dev'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/rnwood/smtp4dev/releases/download/3.3.1/Rnwood.Smtp4dev-win-x64-3.3.1.zip'
+$url        = 'https://github.com/rnwood/smtp4dev/releases/download/3.3.2/Rnwood.Smtp4dev-win-x64-3.3.2.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   softwareName  = 'smtp4dev*'
   url           = $url
 
-  checksum      = '07D666F4E58A96A54D597B141B374C196639888CE30A7F80BAC6D703E71CD168'
+  checksum      = 'C21F70BB3FD759FB2D66DDE47AB3F4CEEA442223DEE02E516B9289451508335C'
   checksumType  = 'sha256'
 }
 
