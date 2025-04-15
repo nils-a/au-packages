faa0bd800852bd8d6a4641bc819bd981aded2c36
Chocolatey <chocolatey@realdimensions.net>
2025-04-15 07:41:13 +0000

    AU: 2 updated - lazygit smtp4dev [skip ci] https://gist.github.com/nils-a/be944da6cbb50300f2e3114bf35724a0/794c9b24107ba37074259b453373b186f8539d2e

--- lazygit/tools/chocolateyinstall.ps1
+++ lazygit/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url           = 'https://github.com/jesseduffield/lazygit/releases/download/v0.48.0/lazygit_0.48.0_Windows_32-bit.zip'
-    url64bit      = 'https://github.com/jesseduffield/lazygit/releases/download/v0.48.0/lazygit_0.48.0_Windows_x86_64.zip'
-    checksum      = '9F0480378151CC91A62E412FB8852B8B5CF4E8F66C70FCBABB33275278223359'
-    checksum64    = '26BA0970FC399075826F8612DFDA39FE9E25114337264A0DC810C09CD25A758C'
+    url           = 'https://github.com/jesseduffield/lazygit/releases/download/v0.49.0/lazygit_0.49.0_Windows_32-bit.zip'
+    url64bit      = 'https://github.com/jesseduffield/lazygit/releases/download/v0.49.0/lazygit_0.49.0_Windows_x86_64.zip'
+    checksum      = '70719F47208890576407A83CE310C07EE0F64B275041ED09D8347E1D407D241F'
+    checksum64    = 'C93E2937A039F0FEA984BB6738E8D6130E6580FE5916C8BED7220CDCE34FCEE5'
     checksumType  = 'sha256'
 }
 
--- smtp4dev/tools/chocolateyinstall.ps1
+++ smtp4dev/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'smtp4dev'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/rnwood/smtp4dev/releases/download/3.8.3/Rnwood.Smtp4dev-win-x64-3.8.3.zip'
+$url        = 'https://github.com/rnwood/smtp4dev/releases/download/3.8.4/Rnwood.Smtp4dev-win-x64-3.8.4.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   softwareName  = 'smtp4dev*'
   url           = $url
 
-  checksum      = 'DCF4DC503194522943EF99ACA9F4D121AFD3088CFA9C01EA2EB748E90C9E42B1'
+  checksum      = '05951606236AE07CCDFEDC8AC4E30BA4AAC9B481B5946B33B618B860188DB17E'
   checksumType  = 'sha256'
 }
 
