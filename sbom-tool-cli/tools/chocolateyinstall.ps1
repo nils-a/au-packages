72318c5e69c1c9736beae660a902e3a37b274cdd
Nils Andresen <nils@nils-andresen.de>
2026-01-12 10:26:06 +0100

    [skip-ci] sbom-tool-cli: 4.0.2

--- sbom-tool-cli/tools/chocolateyinstall.ps1
+++ sbom-tool-cli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $target = Join-Path -Path $toolsDir -ChildPath 'sbom-tool-win-x64.exe'
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     fileFullPath  = $target
-    url           = 'https://github.com/microsoft/sbom-tool/releases/download/v3.0.0/sbom-tool-win-x64.exe'
-    checksum      = 'A765619675E48E3D4A029C5B30907BAEB7EF1C90A3D4FAA1290A3D0D763B6251'
+    url           = 'https://github.com/microsoft/sbom-tool/releases/download/v4.0.2/sbom-tool-win-x64.exe'
+    checksum      = '3FE29379138A43BF99A076AD1E60EFE6C0DFA1EEB0933141EF751EE77ADBDBD7'
     checksumType  = 'sha256'
 }
 
